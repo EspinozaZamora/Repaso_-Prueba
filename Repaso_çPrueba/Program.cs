c8f649875432aed70e661e0149274f052a362255
Estudiante PUCESE <estudiante@pucese.edu.ec>
2019-01-03 09:24:11 -0500

    Solucion 1- Examen.

--- Repaso_çPrueba/Program.cs
+++ Repaso_çPrueba/Program.cs
@@ -8,8 +8,83 @@ namespace Repaso_çPrueba
 {
     class Program
     {
+        const int MAX = 100;
         static void Main(string[] args)
         {
+            int[] vector = new int[MAX];
+            LlenarVector(vector);
+            ordenarVecor(vector);
+            ImprimirVector(vector);
+            Console.WriteLine("-Valor Maximo: {0} ", vector[0]);
+            Console.WriteLine("-Valor Minimo: {0} ", vector[MAX-1]);
+            Console.WriteLine("-Sumatoria: {0} ", Sumatoria(vector));
+            Console.WriteLine("-Promedio: {0} ", Promedio(vector));
+            Console.WriteLine("-Total Decenas: {0} ", ContarDecenas(vector));
+
+            Console.ReadKey();
+
+        }
+        public static int ContarDecenas(int[] x)
+        {
+            int c = 0;
+            for(int i = 0; i < MAX; i++)
+            {
+                if (x[i] >= 10 && x[i] <= 90)
+                {
+                    if (x[i] % 10 == 0)
+                        c++;
+                }
+            }
+            return c;
+        }
+
+        public static int Sumatoria(int[] x)
+        {
+            int suma = 0;
+            for (int i = 0; i < MAX; i++)
+                suma = suma + x[i];
+
+            return suma;
+        }
+        public static double Promedio(int[] x)
+        {
+            double suma = Sumatoria(x);
+            return (suma / MAX);
+        }
+        public static void LlenarVector(int[] x)
+        {
+            Random r = new Random();
+            for (int i = 0; i < MAX; i++)
+            {
+                int numero = r.Next(1, 1000);
+                    x[i] = numero;
+            }
+
+        }
+        public static void ImprimirVector(int[] x)
+        {
+            Console.WriteLine("Elementos del vector: ");
+            for(int i=0; i < MAX; i++)
+            {
+                Console.Write(" {0}        ", x[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void ordenarVecor(int[] x)
+        {
+            int aux = 0;
+            for(int i = 0; i < MAX; i++)
+                for (int j = i + 1; j < MAX; j++)
+                {
+                    if (x[i] < x[j])
+                    {// intercambio
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }
+                }
+
+
         }
     }
 }
